33107c2d39d3974788ace8a55946abf72cb31d85
vitkovic <nvitko@gmail.com>
2023-06-06 12:46:21 +0200

    sb1

---
+++ myapp/src/test/java/com/example/demo/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
